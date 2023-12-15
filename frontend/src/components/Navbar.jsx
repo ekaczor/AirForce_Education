61537c187aab766e89be21241ec0b85b55023d4e
Francisco Alvarado <fjalvarado.1.2001@gmail.com>
2023-12-15 13:14:44 -0500

    makingNavBarResponsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,39 @@
 import React from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { useRef } from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "../style/NavStyle.css";
 
 // Navbar component responsible for rendering the navigation bar.
-const Navbar = () => {
-  // Log a message when the Navbar component is rendered.
-  console.log("Navbar rendered");
+function Navbar() {
+	const navRef = useRef();
 
-  return (
-    <nav className="nav">
-      {/* Link to the home page with a site title */}
-      <Link to="/HomePage" className="site-title">
-        Name of Page
-      </Link>
-      <ul>
-        {/* CustomLink components for navigation to ContactPage and AboutPage */}
-        <CustomLink to="/ContactPage">Contact us</CustomLink>
-        <CustomLink to="/AboutPage">About us</CustomLink>
-      </ul>
-    </nav>
-  );
-};
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
 
-// CustomLink component for rendering navigation links with active state
-function CustomLink({ to, children, ...props }) {
-  // Resolve the path using the useResolvedPath hook
-  const resolvedPath = useResolvedPath(to);
-  
-  // Check if the current route matches the resolved path using useMatch
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-  // Render a list item with an active class based on the isActive state
-  return (
-    <li className={isActive ? "active" : ""}>
-      {/* Render a Link component for navigation */}
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<Link to="/HomePage">Home Page</Link>
+				<Link to="/ContactPage">Contact us</Link>
+				<Link to="/AboutPage">About us</Link>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
 
-// Export the Navbar component as the default export
 export default Navbar;
