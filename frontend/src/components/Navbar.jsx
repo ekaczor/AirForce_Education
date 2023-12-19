14207fddc6e888278d4bca8a7b5187bac9b0f508
Evan Kaczor <evankaczor@icloud.com>
2023-12-19 14:34:45 -0500

    Buttons and info put in home page, created tool cards, added jet logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,38 @@
-import React from "react";
 import { useRef } from "react";
-import {FaBars, FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../style/NavStyle.css";
 
 // Navbar component responsible for rendering the navigation bar.
 function Navbar() {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	return (
-		<header>
-			<h3>LOGO</h3>
-			<nav ref={navRef}>
-				<Link to="/HomePage">Home Page</Link>
-				<Link to="/ContactPage">Contact us</Link>
-				<Link to="/AboutPage">About us</Link>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+  return (
+    <header>
+      <h3>
+        <img
+          src="src/assets/Airplane.png"
+          alt="Company logo"
+          className="logo"
+        />
+      </h3>
+      <nav ref={navRef}>
+        <Link to="/HomePage">Home Page</Link>
+        <Link to="/ContactPage">Contact us</Link>
+        <Link to="/AboutPage">About us</Link>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
